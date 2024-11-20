664bc6887e2720ca0b8552835d672c88f740146e
shimanto-Web-Dev <shimantowebdeveloper@gmail.com>
2024-11-20 23:22:03 +0600

    new arrivals

--- src/Components/NewArrivals.jsx
+++ src/Components/NewArrivals.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NewArrivals = () => {
   return (
@@ -7,6 +8,19 @@ const NewArrivals = () => {
         <div className="container">
           <div className="row">
             <img src="images/newarrivals.png" alt="new arrivals" />
+            <div className='flex justify-between mt-[93.09px]'>
+              <div>
+              <img src="images/newarrivals1.png" alt="newarrivals" />
+              <p className='text-[23.4px] font-medium font-poppins text-[#191919] leading-[36.6px] tracking-[-4%]'>Hoodies & Sweetshirt</p>
+              <Link>Explore Now!</Link>
+              </div>
+              <div>
+              <img src="images/newarrivals2.png" alt="newarrivals" />
+              </div>
+              <div>
+              <img src="images/newarrivals3.png" alt="newarrivals" />
+              </div>
+            </div>
           </div>
         </div>
       </section>
