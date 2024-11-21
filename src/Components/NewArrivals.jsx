4c02f0b436c526d61a6c85711f6df8d92e705e11
shimanto-Web-Dev <shimantowebdeveloper@gmail.com>
2024-11-21 18:59:13 +0600

    new arrivals

--- src/Components/NewArrivals.jsx
+++ src/Components/NewArrivals.jsx
@@ -11,14 +11,18 @@ const NewArrivals = () => {
             <div className='flex justify-between mt-[93.09px]'>
               <div>
               <img src="images/newarrivals1.png" alt="newarrivals" />
-              <p className='text-[23.4px] font-medium font-poppins text-[#191919] leading-[36.6px] tracking-[-4%]'>Hoodies & Sweetshirt</p>
-              <Link>Explore Now!</Link>
+              <p className='text-[23.4px] font-medium font-poppins text-[#191919] leading-[36.6px] tracking-[-4%] mt-[20.47px]'>Hoodies & Sweetshirt</p>
+              <Link className='text-[17.55px] font-medium font-poppins text-[#7F7F7F] leading-[36.6px] tracking-[-4%] '>Explore Now!</Link>
               </div>
               <div>
               <img src="images/newarrivals2.png" alt="newarrivals" />
+              <p className='text-[23.4px] font-medium font-poppins text-[#191919] leading-[36.6px] tracking-[-4%] mt-[20.47px]'>Coats & Parkas</p>
+              <Link className='text-[17.55px] font-medium font-poppins text-[#7F7F7F] leading-[36.6px] tracking-[-4%] '>Explore Now!</Link>
               </div>
               <div>
               <img src="images/newarrivals3.png" alt="newarrivals" />
+              <p className='text-[23.4px] font-medium font-poppins text-[#191919] leading-[36.6px] tracking-[-4%] mt-[20.47px]'>Tees & T-Shirt</p>
+              <Link className='text-[17.55px] font-medium font-poppins text-[#7F7F7F] leading-[36.6px] tracking-[-4%] '>Explore Now!</Link>
               </div>
             </div>
           </div>
